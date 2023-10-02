3bf13c453b5cc2c0212e0dc67ea33a61bbf46773
somak01 <61352713+somak01@users.noreply.github.com>
2023-10-02 07:21:18 +0200

    Update done!

--- public/js/editFormPop.js
+++ public/js/editFormPop.js
@@ -5,18 +5,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const hiddenForm = document.querySelector("#hiddenForm");
     const deleteButton = document.querySelector("#deleteButton");
     const selectElement = document.querySelector("[name='activitySelect']")
+    const dateField = document.querySelector("[name='date']");
+    const activityField = document.querySelector("[name='activity']");
+    const locationField = document.querySelector("[name='location']");
+    const saveButton = document.querySelector("#saveButton");
 
     
     deleteButton.addEventListener("click", async (event) => {
         event.preventDefault()
         try {
             const selectedOption = selectElement.options[selectElement.selectedIndex];
-            const selectedId = selectedOption.id;
-            const response = await axios.delete(`/modify/delete/${selectedId}`);
-
-            if (response.status === 200) {
-                console.log("Finally!!!");
-                window.location.reload();
+            if (selectedOption) {
+                const selectedId = selectedOption.id;
+                console.log("This?");
+                const response = await axios.delete(`/modify/delete/${selectedId}`);
+                if (response.status === 200) {
+                    console.log("Finally!!!");
+                    window.location.reload();
+                }
+            } else {
+                alert("There's nothing to delete!!!");
             }
             
         } catch (error) {
@@ -27,6 +35,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
     editButton.addEventListener("click", async (event) => {
         event.preventDefault();
-        hiddenForm.style.display = "block";
+        try {
+            const selectedOption = selectElement.options[selectElement.selectedIndex];
+            if (selectedOption) {
+                hiddenForm.style.display = "block";
+                const selectedId = selectedOption.id;
+                const response  = await axios.get("/modify/:id/", {params:{id:selectedId}});
+                const {priority, date, activity, location} = response.data;
+                selectedOption.value = priority;
+                activityField.value = activity;
+                locationField.value = location;
+                dateField.value = date.slice(0, 10);
+            } else {
+                alert("There's nothing to edit!!!");
+            }
+        } catch(error) {
+            console.error(error);
+        }
+    })
+
+
+
+    saveButton.addEventListener("click",async (event) => {
+        event.preventDefault();
+        const selectedOption = selectElement.options[selectElement.selectedIndex];
+        const selectedId = selectedOption.id;
+        const activity = activityField.value;
+        const priority = selectedOption.value;
+        const date = dateField.value;
+        const location = locationField.value;
+        try{
+            if (selectedId && activity && priority && date && location) {
+                const response = await axios.put(`/modify/update/${selectedId}`, {
+                    priority:priority,
+                    date: date,
+                    activity:activity,
+                    location:location,
+                });
+                if (response.status === 200) {
+                    alert("Succesfull update!");
+                    window.location.reload();
+                }
+            }
+        } catch(error) {
+            console.error(error);
+        }
+         
     })
 })
--- routes/modify.js
+++ routes/modify.js
@@ -20,3 +20,39 @@ modifyRouter.get("/", async (req, res, next) => {
     const todos = await Todo.find();
     return res.render("modify", {todo:todos});
 });
+
+
+modifyRouter.get("/:id", async (req, res, next) => {
+    //console.log(req.query);
+    const instance = await Todo.findById(req.query.id);
+    res.json(instance);
+})
+
+
+modifyRouter.put("/update/:id/", async (req, res, next) => {
+    //const instance = await Todo.findById(req.query.id);
+    console.log(req.body);
+    const {priority, date, activity, location} = req.body;
+    //console.log(instance);
+    const filter = {_id:req.params.id};
+    const replacement = {
+        priority:priority,
+        date:date,
+        activity:activity,
+        location:location
+    };
+    const inst = await Todo.find(filter);
+    console.log(inst);
+    const result = await Todo.updateOne(filter, replacement);
+    //Todo.save();
+    console.log(result);
+    if (result.nModified === 1) {
+        console.log("Yess!");
+    }
+    //result.save();
+
+
+    res.status(200).send();
+    // instance.priority = 
+    // instance.date = 
+})
